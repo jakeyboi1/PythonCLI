9248d0af9f682012b7d0acf9de8751dcfba199d6
Jake Landrum <jakelandrum2004@gmail.com>
2023-10-04 22:35:39 -0500

    optimization

--- services/epicGamesWebScraper.py
+++ services/epicGamesWebScraper.py
@@ -4,22 +4,10 @@ import json
 def viewFreeGamesOfTheWeek():
     r = requests.get('https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions', headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"})
     loadedData = json.loads(r.content)
-    if 'data' in loadedData:
-        if 'Catalog' in loadedData['data']:
-            if 'searchStore' in loadedData['data']['Catalog']:
-                if 'elements' in loadedData['data']['Catalog']['searchStore']:
-                    for index in loadedData['data']['Catalog']['searchStore']['elements']:
-                        #print(index) use this to see the data
-                        if 'price' in index:
-                            if 'totalPrice' in index['price']:
-                                if 'discountPrice' in index['price']['totalPrice']:
-                                    if int(index['price']['totalPrice']['discountPrice']) == 0:
-                                        if 'categories' in index:
-                                            for k in index['categories']:
-                                                if 'path' in k:
-                                                    if k['path'] == 'freegames':
-                                                        if index['title'] != 'PAYDAY 2':
-                                                            print("Free Game: " + index['title'] )
+    if loadedData["data"]["Catalog"]["searchStore"]["elements"]:
+        for index in loadedData['data']['Catalog']['searchStore']['elements']:
+            if index["price"]["totalPrice"]["discountPrice"] == 0 and index["title"] != "PAYDAY 2":
+                print("Free Game: " + index['title'] )
 
     print('The games listed above are the free games of the week on Epic Games! Press "ENTER" at any time to close this program.')
     input()
